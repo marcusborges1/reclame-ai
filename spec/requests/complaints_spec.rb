891105f815ef0730b16e7921da0b1ef16f15ca00
Marcus Vinicius da Silva Borges <marcus.f22@gmail.com>
2019-10-24 10:58:06 -0300

    adds request spec to Complaints API.

--- spec/requests/complaints_spec.rb
+++ spec/requests/complaints_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Complaints API', type: :request do
   describe 'GET /' do
+    before { get '/' }
+
+    it 'returns a successful response' do
+      expect(response).to be_successful
+    end
+
     it 'returns a sucessful response' do
-      get '/'
       expect(response.body).to include('Reclame aí')
     end
   end
