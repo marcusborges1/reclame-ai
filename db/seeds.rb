1b174f08ac765a380156c95c9708277238991578
Marcus Vinicius da Silva Borges <marcus.f22@gmail.com>
2019-10-26 11:11:15 -0300

    adds seeds data to project.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+first_item, second_item, third_item = Item.create!([
+  {
+    name: 'Headset Gamer HyperX Cloud Stinger - HX-HSCS-BK/LA',
+    description: 'Headset muito bom',
+    barcode: '740617255485',
+    price: 231_47,
+    available_quantity: 452
+  },
+  {
+    name: 'Memória HyperX Fury, 8GB, 2400MHz, DDR4, CL15, Preto - HX424C15FB3/8',
+    description: 'Memória bom para um PC gamer',
+    barcode: '740617241044',
+    price: 221_06,
+    available_quantity: 1431
+  },
+  {
+    name: 'Monitor Gamer AOC Hero LED 24 Widescreen, Full HD, HDMI/VGA/DVI/DP',
+    description: 'Bom pra quem jogar FPS',
+    barcode: '7898930919362',
+    price: 1376_35,
+    available_quantity: 41
+  }
+])
+
+first_sale, second_sale = Sale.create!([
+  {
+    order_number: '123',
+    total_value: 231_47
+  },
+  {
+    order_number: '456',
+    total_value: 1818_47
+  }
+])
+
+SubSale.create!([
+  {
+    item: first_item,
+    sale: first_sale,
+    quantity: 1,
+    subtotal: 231_47
+  },
+  {
+    item: second_item,
+    sale: second_sale,
+    quantity: 2,
+    subtotal: 442_12
+  },
+  {
+    item: third_item,
+    sale: second_sale,
+    quantity: 1,
+    subtotal: 1818_47
+  }
+])
