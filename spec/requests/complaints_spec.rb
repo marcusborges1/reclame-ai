94bd2d90baa7cc4be522ad27f633a2a6bcf67f3c
Marcus Vinicius da Silva Borges <marcus.f22@gmail.com>
2019-10-26 15:46:50 -0300

    creates request spec of complaints to not create a complaint without a valid order_number.

--- spec/requests/complaints_spec.rb
+++ spec/requests/complaints_spec.rb
@@ -40,5 +40,25 @@ RSpec.describe 'Complaints API', type: :request do
         expect(response).to redirect_to(root_path)
       end
     end
+
+    context 'with invalid order_number' do
+      let(:invalid_attributes) {
+        {
+          complaint: {
+            name: 'Uriel Silva',
+            email: 'uriel@foo.bar',
+            phone_number: '61912345678',
+            order_number: 'inexistent_order_number',
+            delivery_cep: '70364400',
+            description: 'My product is not working :('
+          }
+        }
+      }
+
+      it 'does not create a new complaint' do
+        expect { post '/complaints', params: invalid_attributes }
+          .to_not change { Complaint.count }
+      end
+    end
   end
 end
