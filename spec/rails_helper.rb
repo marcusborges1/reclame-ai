a604455fe4bf495495c81b55fa3b064890f59b3f
Marcus Vinicius da Silva Borges <marcus.f22@gmail.com>
2019-10-28 21:07:07 -0300

    adds webmock to rails_helper.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'database_cleaner'
-
+require 'webmock/rspec'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -53,7 +53,7 @@ RSpec.configure do |config|
   # add 'FactoryBot' methods
   config.include FactoryBot::Syntax::Methods
 
-  # start by trucating all the table but then use faster transaction strategy the 
+  # start by trucating all the table but then use faster transaction strategy the
   # rest of the time
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
