5a2c4be94ac829b54c8d63cd3d3436f31c2d37c7
Marcus Vinicius da Silva Borges <marcus.f22@gmail.com>
2019-10-29 22:26:33 -0300

    creates DetermineComplaintSafetyJob#coordinates_of(delivery_cep).

--- app/jobs/determine_complaint_safety_job.rb
+++ app/jobs/determine_complaint_safety_job.rb
@@ -1,6 +1,12 @@
 class DetermineComplaintSafetyJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  def coordinates_of(delivery_cep)
+    location = ViaCep::Address.new(delivery_cep)
+    query = "#{location.state} #{location.city} #{location.neighborhood} #{location.address}"
+    geocoder_location = Geocoder.search(query).first.coordinates
+  end
+
+  def perform(delivery_cep, complaint)
   end
 end
--- spec/jobs/determine_complaint_safety_job_spec.rb
+++ spec/jobs/determine_complaint_safety_job_spec.rb
@@ -1,6 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe DetermineComplaintSafetyJob, type: :job do
+  describe '#coordinates_of(delivery_cep)' do
+    let(:delivery_cep) { '01001000' }
+
+    before do
+      stub_request(:get, "https://viacep.com.br/ws/#{delivery_cep}/json").
+        to_return(body: '{
+          "cep": "01001-000",
+          "logradouro": "Praça da Sé",
+          "complemento": "lado ímpar",
+          "bairro": "Sé",
+          "localidade": "São Paulo",
+          "uf": "SP",
+          "unidade": "",
+          "ibge": "3550308",
+          "gia": "1004"
+        }')
+    end
+
+    it 'returns an array with [latitude, longitude] of given cep' do
+      job = DetermineComplaintSafetyJob.new
+      expect(job.coordinates_of(delivery_cep)).to match_array([-23.5511736, -46.6340015])
+    end
+  end
+
   describe '#perform(*args)' do
   end
 end
