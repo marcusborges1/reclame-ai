937c490e9c1c4c22d88d2c6bc58f5cab0de5b4ca
Marcus Vinicius da Silva Borges <marcus.f22@gmail.com>
2019-10-29 22:52:02 -0300

    adds case of invalid cep to #coordinates_of(delivery_cep) spec.

--- spec/jobs/determine_complaint_safety_job_spec.rb
+++ spec/jobs/determine_complaint_safety_job_spec.rb
@@ -2,26 +2,42 @@ require 'rails_helper'
 
 RSpec.describe DetermineComplaintSafetyJob, type: :job do
   describe '#coordinates_of(delivery_cep)' do
-    let(:delivery_cep) { '01001000' }
+    context 'with a valid cep' do
+      let(:delivery_cep) { '01001000' }
 
-    before do
-      stub_request(:get, "https://viacep.com.br/ws/#{delivery_cep}/json").
-        to_return(body: '{
-          "cep": "01001-000",
-          "logradouro": "Praça da Sé",
-          "complemento": "lado ímpar",
-          "bairro": "Sé",
-          "localidade": "São Paulo",
-          "uf": "SP",
-          "unidade": "",
-          "ibge": "3550308",
-          "gia": "1004"
-        }')
+      before do
+        stub_request(:get, "https://viacep.com.br/ws/#{delivery_cep}/json").
+          to_return(body: '{
+            "cep": "01001-000",
+            "logradouro": "Praça da Sé",
+            "complemento": "lado ímpar",
+            "bairro": "Sé",
+            "localidade": "São Paulo",
+            "uf": "SP",
+            "unidade": "",
+            "ibge": "3550308",
+            "gia": "1004"
+          }')
+      end
+
+      it 'returns an array with [latitude, longitude] of given cep' do
+        job = DetermineComplaintSafetyJob.new
+        expect(job.coordinates_of(delivery_cep)).to match_array([-23.5511736, -46.6340015])
+      end
     end
 
-    it 'returns an array with [latitude, longitude] of given cep' do
-      job = DetermineComplaintSafetyJob.new
-      expect(job.coordinates_of(delivery_cep)).to match_array([-23.5511736, -46.6340015])
+    context 'with an invalid cep' do
+      let(:delivery_cep) { '12345678' }
+
+      before do
+        stub_request(:get, "https://viacep.com.br/ws/#{delivery_cep}/json").
+          to_return(body: '{"erro": true}')
+      end
+
+      it 'raises an ViaCep::ApiRequestError' do
+        job = DetermineComplaintSafetyJob.new
+        expect { job.coordinates_of(delivery_cep) }.to raise_error(ViaCep::ApiRequestError)
+      end
     end
   end
 
